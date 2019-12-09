62101d3b381bbf0a56e60729124746169c343d3c
Shubham Kumar <writetoshubham@gmail.com>
2019-12-09 14:57:24 +0530

    Initial Project commit

---
+++ movie-info-servie/src/main/java/com/shubham/movieinfoservie/MovieInfoServieApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.movieinfoservie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServieApplication.class, args);
+	}
+
+}
---
+++ movie-info-servie/src/test/java/com/shubham/movieinfoservie/MovieInfoServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.movieinfoservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
